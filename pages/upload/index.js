2015da0c65465559e650db5185d4744e72b4295f
Manuel Javier Miguel <manuel.miguel@adevinta.com>
2024-01-04 12:21:38 +0100

    Preparing page for uploading invoices

---
+++ pages/upload/index.js
@@ -0,0 +1,11 @@
+import {Box} from '@mui/material'
+
+import FileUploader from '../../components/fileUploader'
+
+export default function UploadInvoice() {
+  return (
+    <Box>
+      <FileUploader />
+    </Box>
+  )
+}
